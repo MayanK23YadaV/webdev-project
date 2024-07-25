1089d206ff22e023165969a4d01f75d5fb895728
MayanK23YadaV <lovelifenature47@gmail.com>
2024-07-25 20:07:46 +0530

    25.6 8:08

--- Calculate the Factorial/script.js
+++ Calculate the Factorial/script.js
@@ -1,7 +1,8 @@
 function calculate(){
     let F = document.getElementById("input").value
-    fact=parseFloat(F)
-    let result;
-    result=fact+10
+    let fact=parseFloat(F)
+    let result = 1;
+    for (let i=1 ; i<=fact ;i++){
+    result *=i;}
     document.getElementById("result").innerHTML = "result == "+ result;
 }
